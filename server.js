aaed0e7eaac5a38df91e4778e34435cf6bf3ffe8
Aplex Studios <ayanajaz638@gmail.com>
2026-06-05 15:06:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const { PeerServer } = require('peer');
 
 const peerServer = PeerServer({
   port: process.env.PORT || 9000,
-  path: '/'
+  path: '/',
+  proxied: true,
+  key: process.env.PEER_KEY || 'aplex804404'
 });
 
 console.log('PeerJS Server running!');
